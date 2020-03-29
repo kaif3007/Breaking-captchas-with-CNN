84d9fbdc80c41575fb3920d927af84e401cbbd9e
Md kaif khan <35622849+kaif3007@users.noreply.github.com>
2020-03-29 22:31:57 +0530

    Update prog1.py

--- prog1.py
+++ prog1.py
@@ -10,24 +10,35 @@ for (i,imagepath) in enumerate(os.listdir("downloads")):
 	try:
 		path='downloads/'+imagepath
 		image=cv2.imread(path)
-
+		
+                #convert to grayscale.Just 1 channels
 		gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-
+                
+		#increase borders of image by 8 pixels on each side.
 		gray=cv2.copyMakeBorder(gray,8,8,8,8,cv2.BORDER_REPLICATE)
+		
+		#obtain thresholded image.set all pixels greater than 0 to 255 ie white. and remaining black but do its inverse
+		#as method in inverse
 		thresh=cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV|cv2.THRESH_OTSU)[1]
 		
+		#find cotours in image
 		cnts=cv2.findContours(thresh.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 
 		cnts=cnts[0]
+		
+		#sort by decreasing area and pick 4 contours
 		cnts=sorted(cnts,key=cv2.contourArea,reverse=True)[:4]
 
 		for c in cnts:
-			print(c)
+			#approx bounding rectangles for contours
 			(x,y,w,h)=cv2.boundingRect(c)
+			
+			#pick regin of interset
 			roi=gray[y-5:y+h+5,x-5:x+w+5]
 			cv2.imshow("ROI",cv2.resize(roi,(28,28)))
 			key=cv2.waitKey(0)
-
+                        
+			#wait for key by user after that save image in desored directory.
 			if key==ord("'"):
 				print("[INFO] Ignoring character... ")
 				continue
