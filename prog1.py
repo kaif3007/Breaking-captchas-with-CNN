edaed46d8662c85813316f7c29493e0de6957047
Md kaif khan <kaif.17je002751@ee.ism.ac.in>
2020-11-14 18:51:18 +0530

    Update prog1.py

--- prog1.py
+++ prog1.py
@@ -3,6 +3,9 @@ import os
 
 counts={}
 
+#can access pixels as numpy arrays
+#image channels are in (b,g,r) format
+
 for (i,imagepath) in enumerate(os.listdir("downloads")):
 
 	print(f'[INFO] processing image {i+1}/{len(imagepath)}')
@@ -11,17 +14,26 @@ for (i,imagepath) in enumerate(os.listdir("downloads")):
 		path='downloads/'+imagepath
 		image=cv2.imread(path)
 		
-                #convert to grayscale.Just 1 channels
+        #convert to grayscale.Just 1 channels
 		gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
                 
 		#increase borders of image by 8 pixels on each side.
 		gray=cv2.copyMakeBorder(gray,8,8,8,8,cv2.BORDER_REPLICATE)
-		
+
+
+		#thresholding is simply converting grayscale image to binary 0 or 255
 		#obtain thresholded image.set all pixels greater than 0 to 255 ie white. and remaining black but do its inverse
 		#as method in inverse
+		#it returns 2 parameters T,thresh
+		#T is threshold value,thresh is image
+		#In manuaal threshold we need to supply threshold value,whereas is adaptive threshold we don't
 		thresh=cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV|cv2.THRESH_OTSU)[1]
 		
+	
 		#find cotours in image
+		#destroys original image so we pass copy, RETR_EXTERNAL because we only want external contours
+		#find contours works internally by applying edge-detection algorithms
+		#chain-approx-simple only stores boundary points, as no need to store entire points
 		cnts=cv2.findContours(thresh.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 
 		cnts=cnts[0]
